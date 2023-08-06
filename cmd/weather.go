dcf183be6cfc97f9e5979cf3056ec8ef00752632
CRBroughton <crbroughton@posteo.uk>
2023-08-06 12:19:00 +0100

    feat: :sparkles: create getCurrentWeather function

--- cmd/weather.go
+++ cmd/weather.go
@@ -2,6 +2,8 @@ package weather
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -13,8 +15,28 @@ var weatherCmd = &cobra.Command{
 	Long:  "Gets the current weather for the specified location",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		println("hello world")
+		getCurrentWeather()
 	},
+}
+
+func getCurrentWeather() (weather []byte, err error) {
+	url := "https://api.openweathermap.org/data/3.0/onecall?lat=50.8224243265497&lon=-0.13670887945602617&exclude=hourly,daily&appid=X"
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error fetching weather:", err)
+		return []byte(""), err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("Error reading weather response", err)
+		return []byte(""), err
+	}
+	return body, nil
 }
 
 func Execute() {
